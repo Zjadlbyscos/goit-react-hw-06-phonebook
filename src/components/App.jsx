12aafcf7e4b69bd36695a3613d1d2f9540595087
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-11-27 11:29:28 +0100

    update app, delete hooks,crete selectors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,30 @@
-import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import Contacts from './Contacts/Contacts';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 
-import { useAddContact } from 'hooks/useAddContact.js';
-import { useDeleteContact } from 'hooks/useDeleteContact';
-import { useFilterContact } from 'hooks/useFilterContact';
-
 import style from './App.module.css';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Anthony Kiedis', number: '459-12-56' },
-    { id: 'id-2', name: 'Chad Smith', number: '645-17-79' },
-    { id: 'id-3', name: 'Damiano David', number: '645-17-79' },
-  ]);
-  const [filteredContacts, setFilteredContacts] = useState([]);
 
-  const { handleAddContact } = useAddContact({ contacts, setContacts });
-  const { handleDeleteContact } = useDeleteContact({ contacts, setContacts });
-  const { filter, handleFilterChange} =
-    useFilterContact({contacts, setFilteredContacts});
+  const filtered = useSelector(state => state.filter);
+  const contacts = useSelector(state => state.contacts);
+
+  const filterContact = e => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filtered.toLowerCase())
+    );
+    return filteredContacts;
+  };
 
   return (
     <div className={style.form}>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={handleAddContact} setContacts={setContacts} />
+      <ContactForm />
 
-      <Filter value={filter} onChange={handleFilterChange}></Filter>
-      <Contacts contacts={filteredContacts.length < contacts.length ? filteredContacts : contacts} onContactDelete={handleDeleteContact} />
+      <Filter></Filter>
+      <Contacts listContact={filterContact()} />
     </div>
   );
 };
